52c093c194590ef83ce9ec924d168a2d4d6d2a00
ahashans <ahashans@gmail.com>
2019-06-30 21:40:07 +0600

    Removed Parking by plotcode API

--- src/routes/park.js
+++ src/routes/park.js
@@ -40,42 +40,5 @@ router.patch('/park/:id', async (req, res) => {
         
     }
 })
-router.patch('/park', async (req, res) => {
-    try {
-        const _id = req.params.id
-        const plot = await ParkingPlot.findById(_id)
-        if(!plot){
-            return res.status(404).send()
-        }
-        if(plot.booked){
-            const emptyPlot = await ParkingPlot.findOne({booked:false})
-            if(!emptyPlot){
-                return res.status(400).json({
-                    status:"failed",
-                    message:"All parking plot are booked at this moment"
-                })
-            }
-            return res.status(400).json({
-                status:"failed",
-                message:"The plot is booked at this moment! But, plot: "+emptyPlot.code+" is available"
-            })
-            
-        }
-        const updatedPlot = await ParkingPlot.findByIdAndUpdate(req.params.id, {booked:true}, {new:true})
-        if(!updatedPlot){
-            return res.status(500).json({
-                status:"failed",
-                message:"Unable to book the plot. Internal Error"
-            })
-        }
-        return res.status(200).json({
-            status:"Success",
-            message:"The plot is successfully booked!"
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(400).send(error)
-        
-    }
-})
+
 module.exports = router
